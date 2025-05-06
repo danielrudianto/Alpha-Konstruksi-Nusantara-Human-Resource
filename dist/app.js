ca3d0584904a2cb6c771836208af4f692295b639
Daniel Tri <45452945+danielrudianto@users.noreply.github.com>
2025-05-06 15:54:41 +0700

    Update on test, adding t-004

--- dist/app.js
+++ dist/app.js
@@ -15,14 +15,13 @@ const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
 const result_routes_1 = __importDefault(require("./routes/result.routes"));
 const opening_routes_1 = __importDefault(require("./routes/opening.routes"));
 const meet_routes_1 = __importDefault(require("./routes/meet.routes"));
-const socket_io_1 = require("socket.io");
 const http_1 = __importDefault(require("http"));
-const peer_1 = require("peer");
-const rtc_handler_1 = require("./utils/rtc.handler");
+// import { ExpressPeerServer } from "peer";
+// import { RTCHandler } from "./utils/rtc.handler";
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
-const io = new socket_io_1.Server(server);
+// const io = new Server(server);
 app.use((0, cors_1.default)());
 app.use(express_1.default.json({ limit: "10mb" }));
 app.use(express_1.default.urlencoded({ extended: true }));
@@ -34,14 +33,14 @@ app.use("/auth", auth_routes_1.default);
 app.use("/result", result_routes_1.default);
 app.use("/opening", opening_routes_1.default);
 app.use("/meet", meet_routes_1.default);
-app.use("/peerjs", (0, peer_1.ExpressPeerServer)(server));
+// app.use("/peerjs", ExpressPeerServer(server));
 server.listen(5000, () => {
     console.log("Server is running on port 5000.");
-    (0, mongoose_1.connect)("mongodb://localhost:27017/alpha", {}).then(() => {
+    (0, mongoose_1.connect)("mongodb://127.0.0.1:27017/alpha", {}).then(() => {
         console.log("Database is connected.");
     });
 });
-(0, peer_1.ExpressPeerServer)(server).listen(9200, () => {
-    console.log("Peer server is running on port 9200.");
-});
-io.on("connection", rtc_handler_1.RTCHandler);
+// ExpressPeerServer(server).listen(9200, () => {
+//   console.log("Peer server is running on port 9200.");
+// });
+// io.on("connection", RTCHandler);
